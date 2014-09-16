e678ceb79cfa19eee55a1c288a93b600de923646
John Contreras <contrerasnet@gmail.com>
2014-09-16 01:18:22 -0500

    add request phase

--- lib/omniauth/strategies/garmin.rb
+++ lib/omniauth/strategies/garmin.rb
@@ -12,7 +12,7 @@ module OmniAuth
         site: (ENV['GARMIN_CONNECT_API_URL'] || 'http://connectapitest.garmin.com'),
         request_token_path: '/oauth-service-1.0/oauth/request_token',
         access_token_path: '/oauth-service-1.0/oauth/access_token',
-        authorize_url: (ENV['GARMIN_CONNECT_URL'] || 'http://connecttest.garmin.com') + '/oauthConfirm?oauth_callback=localhost:3000'
+        authorize_url: (ENV['GARMIN_CONNECT_URL'] || 'http://connecttest.garmin.com') + '/oauthConfirm'
       }
 
       uid do
@@ -24,6 +24,25 @@ module OmniAuth
           name: access_token.token
         }
       end
+
+
+      def request_phase
+        request_token = consumer.get_request_token({:oauth_callback => "http://localhost:3000" }, options.request_params)
+        session['oauth'] ||= {}
+        session['oauth'][name.to_s] = {'callback_confirmed' => request_token.callback_confirmed?, 'request_token' => request_token.token, 'request_secret' => request_token.secret}
+
+        if request_token.callback_confirmed?
+          redirect request_token.authorize_url(options[:authorize_params])
+        else
+          redirect request_token.authorize_url(options[:authorize_params].merge(:oauth_callback => callback_url))
+        end
+
+      rescue ::Timeout::Error => e
+        fail!(:timeout, e)
+      rescue ::Net::HTTPFatalError, ::OpenSSL::SSL::SSLError => e
+        fail!(:service_unavailable, e)
+      end
+
 
       def consumer
         consumer = GarminConsumer.new(options.consumer_key, options.consumer_secret, options.client_options)
