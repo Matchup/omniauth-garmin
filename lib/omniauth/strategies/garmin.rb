bddcafdb206ec02f842ee309fa24e1f2fa48f3dc
John Contreras <contrerasnet@gmail.com>
2014-10-22 09:48:39 -0500

    remove unneeded code

--- lib/omniauth/strategies/garmin.rb
+++ lib/omniauth/strategies/garmin.rb
@@ -49,26 +49,27 @@ module OmniAuth
       end
 
 
-      def consumer
-        p "in consumer"
+    #   def consumer
+    #     p "in consumer"
 
-        consumer = GarminConsumer.new(options.consumer_key, options.consumer_secret, options.client_options)
-        consumer.http.open_timeout = options.open_timeout if options.open_timeout
-        consumer.http.read_timeout = options.read_timeout if options.read_timeout
-        consumer
-      end
+    #     consumer = GarminConsumer.new(options.consumer_key, options.consumer_secret, options.client_options)
+    #     consumer.http.open_timeout = options.open_timeout if options.open_timeout
+    #     consumer.http.read_timeout = options.read_timeout if options.read_timeout
+    #     consumer
+    #   end
 
-      class GarminConsumer < ::OAuth::Consumer
-        protected
+    #   class GarminConsumer < ::OAuth::Consumer
+    #     protected
 
-        def create_http_request(*params)
-          req = super
-          if ENV['GARMIN_USERNAME']
-            req.basic_auth ENV['GARMIN_USERNAME'], ENV['GARMIN_PASSWORD']
-          end
-          req
-        end
-      end
+    #     def create_http_request(*params)
+    #       req = super
+    #       if ENV['GARMIN_USERNAME']
+    #         req.basic_auth ENV['GARMIN_USERNAME'], ENV['GARMIN_PASSWORD']
+    #       end
+    #       req
+    #     end
+    #   end
+
     end
   end
 end
